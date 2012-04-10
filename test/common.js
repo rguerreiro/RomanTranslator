39b4dc4f2f9325962fd2e0b57a32a0f01e377853
sixspeed <6speedpt@gmail.com>
2012-04-10 16:01:06 +0000

    fixed the infrastructure to run nodeunit tests

--- test/common.js
+++ test/common.js
@@ -1,10 +1,15 @@
 var path = require('path');
 
-exports.run_test = function(filename, test_dir) {
-    if(!test_dir) test_dir = 'test/';
-    
+function BaseTest() {
+}
+
+BaseTest.prototype.run_test = function(filename, test_dir) {
     if(typeof nodeunit === "undefined") {
-        nodeunit = require('nodeunit').reporters.default;
-        nodeunit.run([test_dir + path.basename(filename)]);
+        if(!test_dir) test_dir = 'test/';
+        
+        var _nodeunit = require('nodeunit').reporters.default;
+        _nodeunit.run([test_dir + path.basename(filename)]);
     }
 };
+
+module.exports = new BaseTest();
