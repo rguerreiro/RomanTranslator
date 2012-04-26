9551f91505c85bdd8b35327db5ca7136bc2fc0a3
sixspeed <6speedpt@gmail.com>
2012-04-26 11:13:01 +0000

    adding metatags for SEO #1

--- languages/en.js
+++ languages/en.js
@@ -4,7 +4,7 @@ var Language = require('lingo').Language,
 en.translations = {
   'site title': 'Roman Number Converter',
   'site description': 'This is a simple roman number converter using node.js as backend',
-  'developed by': 'Developed by <a href="https://twitter.com/#!/rguerreiro">@rguerreiro</a> with the sole purpose to sharpen his node.js skills.',
+  'developed by': 'Developed by <a href="https://twitter.com/#!/rguerreiro">@rguerreiro</a> with the sole purpose to sharpen his <a href="http://nodejs.org/">node.js</a> skills.',
   'references': 'Favicon taken from <a href="http://www.famfamfam.com/lab/icons/silk/">famfamfam</a>. Hosted at <a href="http://www.heroku.com/">Heroku</a> and running on <a href="http://expressjs.com/">express.js</a>.',
   'decimal placeholder': 'from decimal...',
   'roman placeholder': 'to decimal...',
--- languages/pt.js
+++ languages/pt.js
@@ -4,7 +4,7 @@ var Language = require('lingo').Language,
 pt.translations = {
   'site title': 'Conversor de Números Romanos',
   'site description': 'Um simples conversor de números romanos construído sobre node.js',
-  'developed by': 'Desenvolvido pelo <a href="https://twitter.com/#!/rguerreiro">@rguerreiro</a> com o intuito de melhorar as suas capacidades de node.js.',
+  'developed by': 'Desenvolvido pelo <a href="https://twitter.com/#!/rguerreiro">@rguerreiro</a> com o intuito de melhorar as suas capacidades de <a href="http://nodejs.org/">node.js</a>.',
   'references': 'Favicon retirado do <a href="http://www.famfamfam.com/lab/icons/silk/">famfamfam</a>. Hospedado no <a href="http://www.heroku.com/">Heroku</a> e a correr sobre <a href="http://expressjs.com/">express.js</a>.',
   'decimal placeholder': 'para romanos...',
   'roman placeholder': 'para decimais...',
